fbe891b70296f3589b2b62ab1e270907ffa3e710
DaminiIntellect <daminiintellectsoftwares.com>
2024-11-12 15:14:25 +0530

    -- ADD changes in main dart.git log

--- lib/mutualfund.dart
+++ lib/mutualfund.dart
@@ -1,4 +1,4 @@
-library mutual_fund.dart;
+// library mutual_fund.dart;
 
 /// screens export
 export 'screens/discover_screen/discover_screen.dart';
@@ -40,3 +40,43 @@ export 'widgets/my_portfolio_list_card.dart';
 export 'widgets/quick_action_card.dart';
 export 'widgets/show_rating_widget.dart';
 export 'widgets/title_and_value_widget.dart';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mutualfund/theme/theme.dart';
+import 'mutualfund.dart';
+import 'routes/routes.dart';
+
+//
+// void main() {
+//   runApp(const MutualFund());
+// }
+
+class MutualFundModule extends StatelessWidget {
+  final ThemeData? theme;
+  final ThemeData? darkTheme;
+  final ThemeMode? themeMode;
+  final String? basUrl;
+
+  const MutualFundModule({
+    super.key,
+    this.theme,
+    this.darkTheme,
+    this.themeMode,
+    this.basUrl = 'www.google.com',
+  });
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Mutual Funds App',
+      getPages: AppRoute.getPages(),
+      debugShowCheckedModeBanner: false,
+      theme: theme ?? AppTheme.lightTheme,
+      darkTheme: darkTheme ?? AppTheme.darkTheme,
+      themeMode: themeMode ?? AppTheme.systemThemeMode,
+      home: const DashBoardScreen(),
+    );
+  }
+}
