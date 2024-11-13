5b895af16b6342467194902c1a333e9f79168a18
SundarIntellect <sundaramoorthy@intellectsoftwares.com>
2024-11-13 11:19:48 +0530

    -- ADD changes in main dart.git log

--- lib/mutualfund.dart
+++ lib/mutualfund.dart
@@ -70,7 +70,7 @@ class MutualFundModule extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Mutual Funds App',
+      title: 'Mutual Funds App new',
       getPages: AppRoute.getPages(),
       debugShowCheckedModeBanner: false,
       theme: theme ?? AppTheme.lightTheme,
